b78aba9625e882633aa23a18988525e44bf691e1
Thanos Angelatos <thanos.angelatos@persado.com>
2014-09-15 12:55:30 +0300

    fix for cloudflare

--- src/main/java/gr/angelatos/support/skroutzfeed/verifier/VerifierMain.java
+++ src/main/java/gr/angelatos/support/skroutzfeed/verifier/VerifierMain.java
@@ -1,6 +1,16 @@
 package gr.angelatos.support.skroutzfeed.verifier;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
+import java.util.zip.GZIPOutputStream;
 
 import nu.xom.Builder;
 import nu.xom.Document;
@@ -13,7 +23,7 @@ public class VerifierMain {
 	private static final String DEFAULTFEED = "http://angelatos.gr/product_feed.php?type=skroutz";
 
 
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Exception {
 		VerifierMain m = new VerifierMain();
 		System.out.println("Feed verifier for Skroutz - http://angelatos.gr open source");
 		if (args.length > 0) {
@@ -23,14 +33,14 @@ public class VerifierMain {
 		}
 	}
 
-	private void verifyFeed(String feed) {
+	private void verifyFeed(String feed) throws Exception{
 		if (feed == null) {
 			feed = DEFAULTFEED;
 		}
 		System.out.println("About to verify the feed from "+feed);
 		try {
 			Builder parser = new Builder();
-			Document doc = parser.build(feed);
+			Document doc = parser.build(retrieveHttpFeed(feed));
 			
 			Nodes products = doc.query("//product");		
 
@@ -49,7 +59,63 @@ public class VerifierMain {
 			ex.printStackTrace();
 		} catch (IOException ex) {
 			System.err.println("Could not connect to the site. The site may be down.");
+			ex.printStackTrace();
 		}
+	}
+
+	private InputStream retrieveHttpFeed(String feed) throws Exception {
+
+		URL url = new URL(feed);
+		long t0 = System.currentTimeMillis();
+
+		URLConnection con = url.openConnection();
+		con.addRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+		con.addRequestProperty("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11");
+		con.setConnectTimeout(30000);
+		con.setDoInput(true);
+		con.setDoOutput(true);
+		con.setReadTimeout(180000);
+		con.connect();
+		System.out.println("Connected to "+feed);
+		for (Map.Entry<String, List<String>> k : con.getHeaderFields().entrySet()) {
+		    for (String v : k.getValue()){
+		         System.out.println(k.getKey() + ":" + v);
+		    }
+		}
+		
+		int bytesRead = 0, totalBytes = 0; 
+		long t1 = 0;
+		InputStream stream = con.getInputStream();
+		ByteArrayOutputStream bout = new ByteArrayOutputStream();
+		
+		GZIPOutputStream gzOut = null;
+		try {
+			gzOut = new GZIPOutputStream(new FileOutputStream(new File("latestfeed.xml.gz")));
+			byte[] byteBuffer = new byte[4096];
+			boolean first = true;
+			while ( (bytesRead = stream.read(byteBuffer)) > -1) {
+				if (first) {
+					t1 = System.currentTimeMillis();
+					first = false;
+				}
+				bout.write(byteBuffer, 0, bytesRead);
+				gzOut.write(byteBuffer, 0, bytesRead);
+				
+				totalBytes += bytesRead;
+				
+				System.out.print("Reading "+totalBytes+" so far... \r");
+			}
+			gzOut.flush();
+			long now = System.currentTimeMillis();
+			System.out.println();
+			System.out.println(" --> waiting time to start getting data : "+(t1-t0)+"ms.");
+			System.out.println(" --> time of download : "+(now-t1)+"ms.");
+			System.out.println("total time : "+(now-t0)+"ms.");
+			
+		} finally {
+			if (gzOut != null) gzOut.close();
+		}
+		return new ByteArrayInputStream(bout.toByteArray());
 	}
 
 	private void printErrorCodes() {
